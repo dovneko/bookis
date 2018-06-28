105b6f63f47894cc7f4e293808c502874ee994e0
dovneko <dov.neko@gmail.com>
2018-06-28 21:31:02 +0900

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+var exec = require('child_process').exec;
+
+var path = '~/_Dev/golang/GOPATH/src/github.com/dovneko/bookis/app/app.yaml';
+
+gulp.task('default',function () {
+    // exec('dev_appserver.py '+path, function (err, stdout, stderr) {
+    //   console.log(stdout);
+    //   console.log(stderr);
+    // });
+  });
